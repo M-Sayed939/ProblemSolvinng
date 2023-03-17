97930154f7020f684d7b1cf6289924896c348611
M-Sayed939 <104098022+M-Sayed939@users.noreply.github.com>
2023-03-18 00:43:00 +0200

    problem A

---
+++ Sheet1/ProblemA.py
@@ -0,0 +1,4 @@
+# Omar is a student in Kindergarten, He is learning to count numbers in class. The Teacher is testing the children's counting of the numbers and he asks them to write from 1to n. Help Omar to answer The question.
+n = int(input())
+for i in range(1, n + 1):
+    print(str(i) + " ")
